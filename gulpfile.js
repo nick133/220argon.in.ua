ea016585da79532f2ad69a4557b9d69bf8a38089
Cage Johnny <Cage Johnny>
2017-02-27 04:34:12 +0200

    Second massive files addition

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+
+const gulp    = require('gulp'),
+      gutil   = require('gulp-util'),
+      glob    = require("glob"),
+      ps      = require('child_process'),
+      plumber = require('gulp-plumber'),
+      stylus  = require('gulp-stylus');
+
+const color = gutil.colors;
+
+const src = {
+    m4:     "src/**/*.m4", // trigger m4 task
+    m4glob: "src/**/*.{styl,stylus,js,jsx,less,sass}.m4", // only feed those to m4
+    styl:   "src/**/*.styl",
+    html:   "src/**/*.html",
+    css:    "src/**/*.css",
+    js:     "src/**/*.js",
+    json:   "src/**/*.json"
+};
+
+
+gulp.task('default', [ 'm4', 'styl', 'watch' ]);
+
+gulp.task('watch',function() {
+    gulp.watch(src.m4,   [ 'm4' ]);
+    gulp.watch(src.styl, [ 'styl' ]);
+
+    return;
+});
+
+gulp.task('m4', function() {
+    glob.sync(src.m4glob).forEach(function(file) {
+        let dest = file.replace('.m4', '');
+
+        gutil.log(color.yellow('m4:'), ' processing ', file, ' -> ', dest);
+        ps.exec('m4 ' + file + '>' + dest);
+    });
+
+    return;
+});
+
+gulp.task('styl', function() {
+    return gulp.src(src.styl)
+        .pipe(plumber())
+        .pipe(stylus())
+        .pipe(gulp.dest('src'));
+});
---
+++ src/hostdefs/hostdefs.js
@@ -0,0 +1,10 @@
+
+
+/*------------------------------*\
+  #HOST-CSS-SHARED-VARIABLES
+\*------------------------------*/
+
+export let defs = {
+  init_color:  "#333",
+  hover_color: "#cc0000"
+};
