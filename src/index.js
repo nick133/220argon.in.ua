550caa35484391cd487b1e934ed6248ad2e7ec9d
Cage Johnny <Cage Johnny>
2017-03-08 05:14:21 +0200

    Refactoring for webpack CSS modules config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import Inferno from 'inferno';
 import Axios   from 'axios';
 
-import Advantages from './Advantages-com/Advantages';
-import Services   from './Services-com/Services';
-import Related    from './Related-com/Related';
-
-import * as Host from './hostlib.js';
+import Advantages from './components/Advantages';
+import Services   from './components/Services';
+import Related    from './components/Related';
 
 import './index.styl';
 
@@ -18,32 +16,43 @@ function browserHotfix() {
     !!navigator.userAgent.match(/Trident/g) ||
     !!navigator.userAgent.match(/MSIE/g) ||
     !!navigator.userAgent.match(/Microsoft Internet Explorer/g);
-  var isChrome = !!window.chrome;
-  var isOpera = window.onoperadetachedviewcontrol !== undefined;
+  // var isChrome = !!window.chrome;
+  // var isOpera = window.onoperadetachedviewcontrol !== undefined;
   
   // You use IE. That´s no good.
   if (isIE) {
     var priceTag = document.getElementById('price-tag');
     priceTag.style.right = '-100px';
-  } else if (isChrome || isOpera) {
-    // Bug with Roboto numeric font weight
-    [ [ 'bolder', [ 'welcome-title', 'related-title', 'services-item__header--open' ]],
-      [ 'bold',   [ 'services-item__header--close' ]]
-    ].forEach(group => group[1].forEach(cls =>
-        document.getElementsByClassName(cls)[0].style.fontWeight = group[0]));
   }
 }
 
+function parseUrlSearch() {
+  let parsed = {};
+
+  window.location.search.substr(1).split('&').map(function(i) {
+    let data = i.split('=');
+    parsed[data[0]] = data[1];
+    return null;
+  });
+
+  return parsed;
+};
  
+
 Axios.get(document.getElementById('host-data').src)
   .then(response => {
-    let uSearch = Host.parseUrlSearch();
+    let uSearch = parseUrlSearch();
+    let scroll = -18 - window
+      .getComputedStyle(document.getElementsByClassName('main-header')[0], null)
+      .getPropertyValue('height')
+      .replace('px', '');
 
     [ [ 'com-advantages', <Advantages data={ response.data.blocks.advantages } /> ],
 
       [ 'com-services', <Services
           openId={ (uSearch.go !== undefined && uSearch.go.length > 1) ? uSearch.go : '' }
           data={ response.data.blocks.services }
+          scroll={ scroll }
         /> ],
 
       [ 'com-related', <Related data={ response.data.blocks.related } /> ],
