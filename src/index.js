e63c747ce8f90971244e03b0edc2bcb11721ab8b
Cage Johnny <Cage Johnny>
2017-03-23 17:14:53 +0200

    New design almost ready, mobile view fix, cleanups

--- src/index.js
+++ src/index.js
@@ -1,88 +1,40 @@
 import './index.styl';
 
-
-// import Inferno from 'inferno';
-// import Axios   from 'axios';
-//
-// import Advantages from './components/Advantages';
-// import Services   from './components/Services';
-// import Related    from './components/Related';
-//
-/*
- *  Hotfix browser related shit
- */
-// function browserHotfix() {
-//   var isIE =
-//     !!navigator.userAgent.match(/Trident/g) ||
-//     !!navigator.userAgent.match(/MSIE/g) ||
-//     !!navigator.userAgent.match(/Microsoft Internet Explorer/g);
-//   // var isChrome = !!window.chrome;
-//   // var isOpera = window.onoperadetachedviewcontrol !== undefined;
-//
-//   // You use IE. That´s no good.
-//   if (isIE) {
-//     var priceTag = document.getElementById('price-tag');
-//     priceTag.style.right = '-100px';
-//   }
-// }
-//
-// function parseUrlSearch() {
-//   let parsed = {};
-//
-//   window.location.search.substr(1).split('&').map(function(i) {
-//     let data = i.split('=');
-//     parsed[data[0]] = data[1];
-//     return null;
-//   });
-//
-//   return parsed;
-// };
-
-[ 'alu' ].forEach(id => {
-  let link  = document.getElementsByClassName('js-link-'  + id)[0];
-  let popup = document.getElementsByClassName('js-popup-' + id)[0];
-
-  link.addEventListener('click', () => {
-    popup.style.display = 'block';
-    popup.style.opacity = 1;
-  });
-  popup.addEventListener('click', () => {
-    popup.style.display = 'none';
-    popup.style.opacity = 0;
-  });
-});
-
-
 /**
  *  Set animation
  */
 
-const elemPhone     = document.getElementById('phone'),
-      elemPhoneLink = document.getElementsByClassName('phone')[0],
-      elemPrice  = document.getElementById('price-tag'),
-      elemWelder = document.getElementById('banner-welder');
+const elemMain   = document.getElementsByClassName('js-main')[0],
+      elemPhone  = document.getElementsByClassName('js-phone')[0],
+      elemPhoneA = document.getElementsByClassName('js-phone-is')[0],
+      elemPrice  = document.getElementsByClassName('js-price-tag')[0],
+      elemWelder = document.getElementsByClassName('js-banner-welder')[0];
 
 const classPrice  = elemPrice.className,
-      classWelder = elemWelder.className;
+      classWelder = elemWelder.className,
+      classPhone  = elemPhone.className;
 
-/* -------- OnScroll phone animation -------- */
-const initPhone = {
-  className: elemPhone.className,
-  color:     elemPhoneLink.style.color,
+/* -------- OnScroll/OnClick phone animation -------- */
+const colorPhone = elemPhoneA.style.color;
+
+const setColorPhone = () => {
+    elemPhoneA.style.color = '#fb0000';
+    setTimeout(() => elemPhoneA.style.color = colorPhone, 800);
 };
 
-elemPhone.addEventListener('animationend', () => {
-  elemPhone.className = initPhone.className;
-  elemPhoneLink.style.color = initPhone.color;
-});
+const animatePhone = () => {
+  if (window.screen.width > 600)
+    elemPhone.className += '  animated  tada';
+  else
+    setColorPhone();
+};
 
-document.addEventListener('wheel', () => {
-  //  elemPhoneLink.style.color = 'blue';
-  elemPhone.className += '  animated  tada';
-});
+elemPhone.addEventListener('animationend', () => elemPhone.className = classPhone);
+elemMain.addEventListener('click', animatePhone);
+document.addEventListener('wheel', animatePhone);
 
 /* -------- Price tag animation -------- */
-elemPrice.addEventListener('animationend',  () => elemPrice.className  = classPrice);
+elemPrice.addEventListener('animationend',  () => elemPrice.className = classPrice);
 
 const animatePrice = () => elemPrice.className += '  animate-price';
 
@@ -96,11 +48,3 @@ elemWelder.addEventListener('animationend', () => elemWelder.className = classWe
 
 elemWelder.addEventListener('click', () => elemWelder.className += '  animate-welder');
 
-
-
-// Axios.get(document.getElementById('host-data').src)
-//   .then(response => {
-//     let uSearch = parseUrlSearch();
-//     browserHotfix();
-//   })
-//   .catch(error => console.log(error));
